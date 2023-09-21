0ce5de6d4e37f551907b1be421145dc2c462ffc5
harisahmed314 <harisahmed314@gmail.com>
2023-09-21 12:15:22 +0500

    register with firebase completed

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,5 +1,9 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react"
 import { Link } from "react-router-dom";
+import { auth } from "../firebase/firebase";
+
+
 
 
 
@@ -9,11 +13,25 @@ export default function RegistrationPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const signUp = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Handle the signed-in user here
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                // Handle the error here
+                console.error("Error signing in:", error.message);
+            });
+    }
     return (
             <div className="mt-4 grow flex items-center justify-around">
                 <div className="mb-64">
                     <h1 className="text-4xl  text-center mb-4">Register</h1>
-                    <form className="max-w-md mx-auto"  >
+                    <form className="max-w-md mx-auto"  onSubmit={signUp} >
                         <input type="text"
                             placeholder="John Doe"
                             value={name}
