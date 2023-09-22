11b72e30cc6473bf981e153745b0d59d8f7b1bc7
harisahmed314 <harisahmed314@gmail.com>
2023-09-22 21:41:16 +0500

    login page set

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl  text-center mb-4">Register</h1>
+                <h1 className="text-4xl  text-center mb-4">Login</h1>
                 <form onSubmit={signIn} className="max-w-md mx-auto">
                     <input 
                         type="email"
@@ -40,7 +40,7 @@ export default function LoginPage() {
                         value={password}
                         onChange={ev => setPassword(ev.target.value)}
                     />
-                    <button type="submit" className="primary">Register</button>
+                    <button type="submit" className="primary">Signin</button>
                     <div className="text-center py-2 text-gray-500">
                         Don't Have an Account? <Link className="underline text-black" to="/register">Register</Link>
                     </div>
