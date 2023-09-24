a9aceb05a727b82eaa88ddab40736d2715b9622b
harisahmed314 <harisahmed314@gmail.com>
2023-09-24 09:49:00 +0500

    changer the render of components setting

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,79 @@ import Layout from '../Layout'
 import RegistrationPage from './pages/RegistrationPage'
 import LoginPage from './pages/LoginPage'
 import { AddProducts } from './pages/AddProduct'
-
-
-
-function App() {
-
-
-
-  return (
-    <>
-    
-      <Routes>
-
-
-        <Route path='/' element={<Layout/>}>
-          <Route path='/register' element={<RegistrationPage/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/addproduct' element={<AddProducts/>} />
-          
-        </Route>
-
-      </Routes>
+import { auth, db } from './config/config'
+import { BrowserRouter } from 'react-router-dom'
+import { ProductsContextProvider } from './Global/productcontext.jsx'
+import Home from './pages/Home'
+import React, { Component } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
 
 
 
 
-    </>
-  )
+
+
+export class App extends Component {
+
+  state = {
+    user: null,
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.getUserInfo(user.uid);
+      } else {
+        this.setState({
+          user: null
+        })
+      }
+    });
+  }
+
+  async getUserInfo(uid) {
+    const userRef = doc(db, 'SignedUpUsersData', uid);
+    const userSnapshot = await getDoc(userRef);
+    if (userSnapshot.exists()) {
+      this.setState({
+        user: userSnapshot.data().Name
+      });
+    }
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <ProductsContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+
+              {/* home */}
+              <Route exact path='/' element={<Home user={this.state.user} />} />
+              {/* signup */}
+              <Route path="/signup" element={<RegistrationPage />} />
+              {/* login */}
+              <Route path="/login" element={<LoginPage />} />
+              {/* cart products */}
+              {/* <Route path="/cartproducts" element={() => <Cart user={this.state.user} />} /> */}
+              {/* add products */}
+              <Route path="/addproducts" element={<AddProducts />} />
+              {/* cashout */}
+              {/* <Route path='/cashout' element={() => <Cashout user={this.state.user} />} />
+                          <Route element={NotFound} /> */}
+
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ProductsContextProvider>
+    )
+  }
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ProductsContextProvider } from './Global/productcontext.jsx'
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-
-  <React.StrictMode>
-    <ProductsContextProvider>
-      <BrowserRouter>
-        <App />
-
-      </BrowserRouter>
-    </ProductsContextProvider>
-  </React.StrictMode>
-
-
-
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
