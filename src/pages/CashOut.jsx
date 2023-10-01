9621b548670cd922a2d39f28549efa5c7a0aa0dd
harisahmed314 <harisahmed314@gmail.com>
2023-10-01 18:11:25 +0500

    cash out page design by tailwind css

--- src/pages/CashOut.jsx
+++ src/pages/CashOut.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../Global/CartContext';
 import { useNavigate } from 'react-router-dom';
 import { auth, db } from '../config/config';
 
-export const Cashout =()=> {
+export const Cashout = () => {
     const navigate = useNavigate();
     const { shoppingCart, totalPrice, totalQty, dispatch } = useContext(CartContext);
 
@@ -31,12 +31,12 @@ export const Cashout =()=> {
         return () => unsubscribe();
     }, [navigate]);
 
-    
+
 
     const cashoutSubmit = async (e) => {
         e.preventDefault();
 
-       
+
 
         try {
             const date = new Date();
@@ -69,40 +69,44 @@ export const Cashout =()=> {
 
     return (
         <>
-            <div className='container'>
-                <br />
-                <h2>Cashout Details</h2>
-                <br />
-                {successMsg && <div className='success-msg'>{successMsg}</div>}
-                <form autoComplete="off" className='form-group' onSubmit={cashoutSubmit}>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className='form-control' required
-                        onChange={(e) => setName(e.target.value)} value={name} />
-                    <br />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" className='form-control' required
-                        value={email} disabled />
-                    <br />
-                    <label htmlFor="Cell No">Cell No</label>
-                    <input type="number" className='form-control' required
-                        onChange={(e) => setCell(e.target.value)} value={cell} placeholder='eg 03123456789' />
-                    <br />
-                    <label htmlFor="Delivery Address">Delivery Address</label>
-                    <input type="text" className='form-control' required
-                        onChange={(e) => setAddress(e.target.value)} value={address} />
-                    <br />
-                    <label htmlFor="Price To Pay">Price To Pay</label>
-                    <input type="number" className='form-control' required
-                        value={totalPrice} disabled />
-                    <br />
-                    <label htmlFor="Total No of Products">Total No of Products</label>
-                    <input type="number" className='form-control' required
-                        value={totalQty} disabled />
-                    <br />
-                    <button type="submit" className='btn btn-success btn-md mybtn'>SUBMIT</button>
+            <div className="container mx-auto px-6 py-8">
+                <h2 className="text-2xl font-semibold text-center mb-6">Cashout Details</h2>
+
+                {successMsg && <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">{successMsg}</div>}
+
+                <form onSubmit={cashoutSubmit} className="space-y-6">
+                    <div>
+                        <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+                        <input type="text" className="mt-1 p-2 w-full border rounded-md" required onChange={(e) => setName(e.target.value)} value={name} />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+                        <input type="email" className="mt-1 p-2 w-full border rounded-md bg-gray-100 cursor-not-allowed" required value={email} disabled />
+                    </div>
+                    <div>
+                        <label htmlFor="Cell No" className="block text-sm font-medium text-gray-700">Cell No</label>
+                        <input type="number" className="mt-1 p-2 w-full border rounded-md" required onChange={(e) => setCell(e.target.value)} value={cell} placeholder='eg 03123456789' />
+                    </div>
+                    <div>
+                        <label htmlFor="Delivery Address" className="block text-sm font-medium text-gray-700">Delivery Address</label>
+                        <input type="text" className="mt-1 p-2 w-full border rounded-md" required onChange={(e) => setAddress(e.target.value)} value={address} />
+                    </div>
+                    <div>
+                        <label htmlFor="Price To Pay" className="block text-sm font-medium text-gray-700">Price To Pay</label>
+                        <input type="number" className="mt-1 p-2 w-full border rounded-md bg-gray-100 cursor-not-allowed" required value={totalPrice} disabled />
+                    </div>
+                    <div>
+                        <label htmlFor="Total No of Products" className="block text-sm font-medium text-gray-700">Total No of Products</label>
+                        <input type="number" className="mt-1 p-2 w-full border rounded-md bg-gray-100 cursor-not-allowed" required value={totalQty} disabled />
+                    </div>
+                    <div>
+                        <button type="submit" className="mt-4 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">SUBMIT</button>
+                    </div>
                 </form>
-                {error && <span className='error-msg'>{error}</span>}
+
+                {error && <span className="block mt-4 text-red-500">{error}</span>}
             </div>
+
         </>
     )
 }
